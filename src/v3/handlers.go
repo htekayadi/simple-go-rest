4489ad6eb07cc4e40558bf5d2389057171f07b38
Hendrik Tekayadi <hendrik@popsical.com>
2017-05-20 23:25:12 +0800

    Logger

--- src/v3/handlers.go
+++ src/v3/handlers.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
 	"fmt"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
-func Index(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(writer, "Welcome!\n")
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome!\n")
 }
 
-func TodoIndex(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(writer, "Todo Index!\n")
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
 }
 
-func TodoShow(writer http.ResponseWriter, request *http.Request) {
-	vars := mux.Vars(request)
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
 	todoId := vars["todoId"]
-	fmt.Fprintf(writer, "Todo show: %s\n", todoId)
+	fmt.Fprintf(w, "Todo show: %s\n", todoId)
 }
