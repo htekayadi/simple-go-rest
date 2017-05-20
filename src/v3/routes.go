acc1a94b92acff6a374cc5b7414c3751a07618cb
Hendrik Tekayadi <hendrik@popsical.com>
2017-05-20 23:28:03 +0800

    Log route handler

--- src/v3/routes.go
+++ src/v3/routes.go
@@ -7,27 +7,35 @@ import (
 )
 
 type Route struct {
-	Name				string
-	Method 			string
-	Pattern			string
+	Name        string
+	Method      string
+	Pattern     string
 	HandlerFunc http.HandlerFunc
 }
 
 type Routes []Route
 
-func Router() *mux.Router {
+func NewRouter() *mux.Router {
+
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
+		var handler http.Handler
+
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
 		router.
 			Methods(route.Method).
 			Path(route.Pattern).
 			Name(route.Name).
-			Handler(route.HandlerFunc)
+			Handler(handler)
+
 	}
+
 	return router
 }
 
-var routes = Routes {
+var routes = Routes{
 	Route{
 		"Index",
 		"GET",
@@ -43,7 +51,7 @@ var routes = Routes {
 	Route{
 		"TodoShow",
 		"GET",
-		"/todos/{todoId",
+		"/todos/{todoId}",
 		TodoShow,
 	},
 }
